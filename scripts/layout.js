ffa63e59d0b437e7ae389beffdf36add64984237
richardlawrence.15below <richardlawrence.15below@c5a23a8d-46a7-e794-a65e-ce57b7db7bff>
2018-08-22 13:44:29 +0000

    stubbing div containers
    git-svn-id: https://github.com/Gaspra/ManyDays/trunk@24 c5a23a8d-46a7-e794-a65e-ce57b7db7bff

--- scripts/layout.js
+++ scripts/layout.js
@@ -13,22 +13,37 @@ $(document).ready(function()
 
 function InitialiseContainers() 
 {
+    var body = $("body");
+    body.css("height", "100vh");
+    body.css("width", "100vw");
+    body.css("padding", "0");
+    body.css("border", "0");
+    body.css("margin", "0");
+    body.css("overflow", "hidden");
+
     manydaysContainer = $("#manydays");
+    manydaysContainer.css("position", "absolute");
 
     aboutContainer = $("#about");
     aboutContainer.height("100%");
     aboutContainer.width("100%");
     aboutContainer.css("background-color", "coral");
+    aboutContainer.css("position", "absolute");
 
     settingsContainer = $("#settings");
+    settingsContainer.css("display", "none");
 
     previewContainer = $("#preview");
-    
+    previewContainer.css("display", "none");
+
     resizeContainer = $("#resize");
-    
+    resizeContainer.css("display", "none");
+
     mapContainer = $("#map");
-    
+    mapContainer.css("display", "none");
+
     galleryContainer = $("#gallery");
+    galleryContainer.css("display", "none");
 }
 
 $(window).resize(function()
