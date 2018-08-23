6d75ab24e95adac25a6f73d68acf2d24bd359bc6
richardlawrence.15below <richardlawrence.15below@c5a23a8d-46a7-e794-a65e-ce57b7db7bff>
2018-08-23 12:47:57 +0000

    whatever promises
    git-svn-id: https://github.com/Gaspra/ManyDays/trunk@43 c5a23a8d-46a7-e794-a65e-ce57b7db7bff

--- scripts/map.js
+++ scripts/map.js
@@ -5,29 +5,23 @@ var manydaysMap;
 
 function InitialiseMap() 
 {
-    loadGoogleApiPromise.push(new Promise(
-        resolve => 
-        {
-            LoadGoogleScript();
-            resolve();
-        }
-    ));
-
-    Promise.all(loadGoogleApiPromise).then(function() 
-    {
-        manydaysMap = new googleKey.maps.Map(document.getElementById("map"), {
-            center: { lat: 1, lng: 1 },
-            zoom: 6,
-            mapTypeId: google.maps.MapTypeId.HYBRID
-        });
-    });
+    LoadGoogleScript();
 }
 
 function LoadGoogleScript()
 {
     googleScript = document.createElement("script");
     googleScript.type = "text/javascript";
-    googleScript.src = "https://maps.googleapis.com/maps/api/js?key="+googleKey;
-    googleScript.async = false;
+    googleScript.src = "https://maps.googleapis.com/maps/api/js?key="+googleKey+"&callback=LoadedGoogleApi";
+}
+
+function LoadedGoogleApi()
+{
     $("#scripts").append(googleScript);
+
+    manydaysMap = new googleKey.maps.Map(document.getElementById("map"), {
+        center: { lat: 1, lng: 1 },
+        zoom: 6,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    });
 }
