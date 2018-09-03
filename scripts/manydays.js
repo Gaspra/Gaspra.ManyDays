220ccba2476e173a03d1db57a6f12ce60a72fa7b
richardlawrence.15below <richardlawrence.15below@c5a23a8d-46a7-e794-a65e-ce57b7db7bff>
2018-09-03 12:23:52 +0000

    testing get json
    git-svn-id: https://github.com/Gaspra/ManyDays/trunk@48 c5a23a8d-46a7-e794-a65e-ce57b7db7bff

--- scripts/manydays.js
+++ scripts/manydays.js
@@ -3,9 +3,23 @@ $(document).ready(function()
     InitialiseContainers();
 
     InitialiseMap();
+
+    LoadImageCollection();
 });
 
 $(window).resize(function() 
 {
     ResizeContainers();
-})
+});
+
+var ImageCollection = {};
+
+function LoadImageCollection() 
+{
+    var promise = $.get('../gallery/manydays.Json');
+    promise.done(function (data) {
+        ImageCollection.Json = JSON.parse(data);
+    });
+
+    ImageCollection.Images = [];
+}
