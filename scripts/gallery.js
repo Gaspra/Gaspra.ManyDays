30b39e135b132fe4d6cc17a7f5488089331ac88e
gaspra <gaspra@c5a23a8d-46a7-e794-a65e-ce57b7db7bff>
2018-11-13 15:28:08 +0000

    Update gallery.js
    git-svn-id: https://github.com/Gaspra/ManyDays/trunk@61 c5a23a8d-46a7-e794-a65e-ce57b7db7bff

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,7 +1,23 @@
-function GetAlbumTest()
+//function GetAlbumTest()
+//{
+//    $.get('https://www.googleapis.com/storage/v1/b/manydays-gallery/o', function(data)
+//    {
+//        console.log(data);
+//    });
+//}
+
+
+function InitialiseGallery()
 {
-    $.get('https://www.googleapis.com/storage/v1/b/manydays-gallery/o', function(data)
-    {
-        console.log(data);
+    ImageCollection.ImageObjects = (JSON.parse(ImageCollection.Json))["Images"];
+
+    ImageCollection.ImageObjects.forEach(function(image) {
+        InitialiseImage(image)
     });
 }
+
+function InitialiseImage(image)
+{
+    console.log(image.Id + ': ' + image.Name + ' ' + image.Location.Name + ' ' + image.Filename);
+}
+
