2c877e6426602c8e455c3caad785510e7ea1680c
gaspra <gaspra@c5a23a8d-46a7-e794-a65e-ce57b7db7bff>
2018-11-14 13:43:42 +0000

    Update gallery.js
    git-svn-id: https://github.com/Gaspra/ManyDays/trunk@67 c5a23a8d-46a7-e794-a65e-ce57b7db7bff

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,21 +1,42 @@
-//function GetAlbumTest()
-//{
-//    $.get('https://www.googleapis.com/storage/v1/b/manydays-gallery/o', function(data)
-//    {
-//        console.log(data);
-//    });
-//}
-
+var googleBucket = "https://storage.googleapis.com/manydays-gallery/";
+var thumbnailBucket = googleBucket + "thumbnail/";
+var previewBucket = googleBucket + "preview/";
+var rawBucket = googleBucket + "raw/";
+var imagePrefix = ".png";
 
 function InitialiseGallery()
 {
-    ImageCollection.Json["Images"].forEach(function(image) {
-        InitialiseImage(image)
+    var numberOfImages = ImageCollection.Json["Images"].length;
+
+    ManyDaysGallery.Promises = [];
+
+    for(var i = numberOfImages - 1; i--; i > -1)
+    {
+        ManyDaysGallery.Promises.push(InitialiseImage(ImageCollection.Json["Images"][i]));
+    }
+
+    Promise.all(ManyDaysGallery.Promises).then(function() {
+        Console.log("all promises done, yey!");
     });
 }
 
 function InitialiseImage(image)
 {
     console.log(image.Id + ': ' + image.Name + ' ' + image.Location.Name + ' ' + image.Filename);
-}
 
+    return new Promise((resolve, reject) => {
+        $('<img/>').attr('src', thumbnailBucket + image.Filename + imagePrefix)
+        .on('load', function ()
+        {
+            $('#gallery').append('<div class="imgThumbnail" id="img_'+image.Id+'"></div>');
+            $('#img_'+image.Id).css('background-image', 'url('+thumbnailBucket + image.Filename + imagePrefi+')');
+
+            resolve();
+        })
+        .on('error', function (err)
+        {
+            console.log('Failed to get image, error: ' + err);
+            resolve();
+        });
+    });
+}
