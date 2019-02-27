0ee33c723b74f3bc52ed912b8e7d275935348e3b
Gaspra <35634732+Gaspra@users.noreply.github.com>
2019-02-27 13:33:00 +0000

    Update layout.js

--- scripts/layout.js
+++ scripts/layout.js
@@ -73,7 +73,7 @@ function InitialiseContainers()
     previewTitle.css("background-color", "rgba(255,255,255,0.9)")
     previewTitle.css("position", "relative");
     previewTitle.css("text-align", "center");
-    previewTitle.css("line-height", "8vh");
+    previewTitle.css("line-height", "6vh");
     previewTitle.css("margin", "auto");
 
     navContainer = $("#nav");
